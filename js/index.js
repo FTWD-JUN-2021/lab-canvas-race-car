c6057833a026dd2b6f55fbe7adccf0a48549c7b3
Mahim Pritom <mahimnyc@yahoo.com>
2021-06-17 17:01:11 -0400

    iteration4 done

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-const canvas = document.querySelector('#canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 
 // canvas.width = 500;
 // canvas.height = 700;
 
-let background = new Image()
-background.src = './images/road.png'
+let background = new Image();
+background.src = "./images/road.png";
 
-background.onload = function() {
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-}
+background.onload = function () {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+};
 
-let racecar = new Image ()
-racecar.src = './images/car.png'
-racecar.onload = function() {
+let racecar = new Image();
+racecar.src = "./images/car.png";
+racecar.onload = function () {
   // ctx.drawImage(racecar, (canvas.width / 2 ) - racecar.width / 6, 575, racecar.width / 3, racecar.height / 3)
   blueCar.draw();
-}
+};
 
 class Car {
   constructor(img, x, y, w, h) {
@@ -26,29 +26,68 @@ class Car {
     this.w = w;
     this.h = h;
   }
-  
+
   draw = () => {
     ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-  }
+  };
 }
 
-let blueCar = new Car(racecar, (canvas.width / 2 ) - racecar.width / 6, 575, racecar.width / 3, racecar.height / 3);
+let blueCar = new Car(
+  racecar,
+  canvas.width / 2 - racecar.width / 6,
+  575,
+  racecar.width / 3,
+  racecar.height / 3
+);
 
-
-window.onkeydown = function(e) {
-  if (e.key === 'ArrowLeft') {
+window.onkeydown = function (e) {
+  if (e.key === "ArrowLeft") {
     blueCar.x -= 10;
-  } 
-  if (e.key === 'ArrowRight') {
+  }
+  if (e.key === "ArrowRight") {
     blueCar.x += 10;
   }
+};
+
+class Obstacle {
+  constructor(x, y, w, h, color) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+  }
+  draw = () => {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  };
+  move = () => {
+    this.y += 1;
+    this.draw();
+  };
 }
+
+const obstacles = [];
+
+setInterval(() => {
+  let obs = new Obstacle(
+    Math.random() * canvas.width,
+    0,
+    Math.random() * 100,
+    10,
+    "red"
+  );
+  obstacles.push(obs);
+}, 1000);
 
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   blueCar.draw();
+  for (let obs of obstacles) {
+    obs.move();
+  }
 }
 
 animate();
