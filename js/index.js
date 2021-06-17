61cafe960224a19381a7911a02255dfd9838fc90
Brandon <BrandonMitchell5365@gmail.com>
2021-06-17 13:26:33 -0700

    iteration 2

--- js/index.js
+++ js/index.js
@@ -10,3 +10,9 @@ background.src = './images/road.png'
 background.onload = function() {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
 }
+
+let racecar = new Image ()
+racecar.src = './images/car.png'
+racecar.onload = function() {
+  ctx.drawImage(racecar, (canvas.width / 2 ) - racecar.width / 6, 575, racecar.width / 3, racecar.height / 3)
+}
